88a0d9516cc962324f1e908e1b7cd58ab9abef05
Toby McLaughlin <toby@jarpy.net>
2020-05-18 20:59:45 +0930

    More tests for Aircraft

--- tests/test_aircraft.py
+++ tests/test_aircraft.py
@@ -1,5 +1,6 @@
 import pyo
 from shakersynth.aircraft.aircraft import Aircraft
+from shakersynth.synth.rotor import RotorSynth
 from pytest import fixture
 
 
@@ -8,6 +9,25 @@ def aircraft():
     synth_engine = pyo.Server(audio="offline_nb")
     synth_engine.boot()
     return Aircraft(synth_engine=synth_engine)
+
+
+def test_new_aircraft_are_not_running(aircraft):
+    assert not aircraft.is_running
+
+
+def test_aircraft_are_running_after_start(aircraft):
+    aircraft.start()
+    assert aircraft.is_running
+
+
+def test_running_aircraft_can_be_stopped(aircraft):
+    aircraft.is_running = True
+    aircraft.stop()
+    assert not aircraft.is_running
+
+
+def test_aircraft_have_a_rotorsynth(aircraft):
+    assert type(aircraft.rotor_synth) is RotorSynth
 
 
 def test_get_rotor_rpm_is_correct_for_mi8(aircraft):
@@ -28,3 +48,8 @@ def test_calculate_rotor_rpm_is_correct_for_huey(aircraft):
     aircraft.update(telemetry)
     rpm = aircraft.get_rotor_rpm()
     assert int(rpm) == 324
+
+
+def test_aircraft_data_key_lookup(aircraft):
+    aircraft.data = {"food": "jp4"}
+    assert aircraft["food"] == "jp4"
