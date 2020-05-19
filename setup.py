1aed29552ba58ecef6233bba1e0f2b02d85e28e9
Toby McLaughlin <toby@jarpy.net>
2020-05-19 23:49:26 +0930

    Set version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="Shakersynth",
-    version="0.0.3",
+    version="0.0.4",
     author="Toby McLaughlin",
     author_email="toby@jarpy.net",
     description="Bass shaker synthesizer for DCS World.",
