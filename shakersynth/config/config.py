1853cfdc378abb96c9cbbd34fd05bbe2b70a6ab4
Toby McLaughlin <toby@jarpy.net>
2020-09-26 07:40:41 +0930

    Increase audio buffer size

--- shakersynth/config/config.py
+++ shakersynth/config/config.py
@@ -9,10 +9,3 @@ except KeyError:
     log_level_name = logging.INFO
 
 global_volume = 0.95
-
-# Only offer to use audio devices that have the following string in their
-# API name.
-if sys.platform == 'Win32':
-    audio_api_filter_string = 'DirectSound'
-else:
-    audio_api_filter_string = 'Pulse'
