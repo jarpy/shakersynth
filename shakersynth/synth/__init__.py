0c71f259d15d3df318fd33cc25dd834b3306f655
Toby McLaughlin <toby@jarpy.net>
2021-07-09 20:49:03 +0930

    Update shakersynth.synth docstring

--- shakersynth/synth/__init__.py
+++ shakersynth/synth/__init__.py
@@ -1,4 +1,10 @@
 """This module contains individual synthesizers for different effects.
 
-A valid synth must implement the `update`, `start`, and `stop` methods.
+A valid synth must take a module name as the sole argument to its constructor.
+It must also implement the `update`, `start`, and `stop` methods.
+
+`update` must accept a telemetry dictionary.
+`start` and `stop` take no arguments.
+
+Refer to `RotorSynth` for an example.
 """
