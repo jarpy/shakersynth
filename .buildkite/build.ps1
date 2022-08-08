142a437be017487c1341911edd6db5c0a2b1ac9d
Toby McLaughlin <toby@jarpy.net>
2022-08-08 19:27:25 +0930

    wip

--- .buildkite/build.ps1
+++ .buildkite/build.ps1
@@ -1,5 +1,7 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install -y python
-refreshenv
+#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+#choco install -y python
+#refreshenv
+
+$Env:PATH += ";C:\Python310\bin;C:\Python310\scripts"
 pip install pytest
 pytest
