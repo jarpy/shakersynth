a39f6a962c79fcff5168f0e3c4aa6d5d6fc79dad
Toby McLaughlin <toby@jarpy.net>
2022-08-08 19:16:35 +0930

    wip

--- .buildkite/build.ps1
+++ .buildkite/build.ps1
@@ -1,4 +1,3 @@
-Remove-Item c:\ProgramData\Chocolatey -Recurse
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install -y python
 refreshenv
