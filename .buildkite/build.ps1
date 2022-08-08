51a3811ae7ac2f98e8091e1ddf9cc81726e657eb
Toby McLaughlin <toby@jarpy.net>
2022-08-08 19:32:15 +0930

    wip

--- .buildkite/build.ps1
+++ .buildkite/build.ps1
@@ -1,7 +1,5 @@
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-#choco install -y python
-#refreshenv
-
-$Env:PATH += ";C:\Python310\bin;C:\Python310\scripts"
+choco install -y python3 --version=3.9.6
+$Env:PATH += ";C:\Python396\bin;C:\Python396\scripts"
 pip install -r requirements.txt
 pytest
